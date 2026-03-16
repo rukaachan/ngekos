7dad6ca0a8bb972d18952bb64137e67cca1c29eb
rukaachan <novops911@gmail.com>
2026-03-16 15:45:12 +0700

    initial

---
+++ app/Models/City.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["image", "name", "slug"];
+
+    // Relation
+    public function boardingHouses()
+    {
+        return $this->hasMany(BoardingHouse::class);
+    }
+}
